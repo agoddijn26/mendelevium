70727bd3b3bc24721ccc26f12d638779726fa61b
agoddijn26 <alexander.goddijn@n26.com>
2019-07-18 15:20:31 +0200

    works as an mvp

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask import request
-import categoriser
+from categoriser import transaction_from_json
+from categoriser import transaction_similarity
 
 app = Flask(__name__)
 
@@ -13,9 +14,9 @@ def hello_world():
 @app.route('/categorise', methods=['POST'])
 def categoriser():
     content = request.get_json()
-    print(content)
-    t1 = co
-    return categoriser.getTestVar()
+    t1 = transaction_from_json(content[0])
+    t2 = transaction_from_json(content[1])
+    return str(transaction_similarity(t1, t2)[0])
 
 
 if __name__ == '__main__':
--- categoriser.py
+++ categoriser.py
@@ -1,13 +1,11 @@
-from sklearn.ensemble import GradientBoostingRegressor
-from sklearn.externals import joblib
-from sklearn.preprocessing import MinMaxScaler
+import joblib
 import numpy as np
 from difflib import SequenceMatcher
 import utils
 
 
 model = joblib.load('categoriser_model.pkl')
-scaler = joblib.load('min_max_scaler.pkl')
+scaler = joblib.load('min_max_scaling.pkl')
 
 
 features = np.array(["same_mcc_group", "merchant_name_similarity", "merchant_name_shared_words",
@@ -15,11 +13,19 @@ features = np.array(["same_mcc_group", "merchant_name_similarity", "merchant_nam
             "reference_text_longest_substring"])
 
 
-class transaction:
+class Transaction:
     def __init__(self, mcc, merchantName, referenceText):
         self.mcc = mcc
         self.merchantName = merchantName
         self.referenceText = referenceText
+
+
+def transaction_from_json(jsonObject):
+    return Transaction(
+        jsonObject['mcc'],
+        jsonObject['merchantName'],
+        jsonObject['referenceText']
+    )
 
 
 def is_same_mcc_group(g1, g2):
@@ -42,8 +48,8 @@ def longest_substring(s1, s2):
     return utils.longest_substring(s1, s2)
 
 
-def transactionSimilarity(t1, t2):
-    X = [
+def transaction_similarity(t1, t2):
+    X = [[
         is_same_mcc_group(t1.mcc, t2.mcc),
         similarity(t1.merchantName, t2.merchantName),
         shared_words(t1.merchantName, t2.merchantName),
@@ -51,6 +57,7 @@ def transactionSimilarity(t1, t2):
         similarity(t1.referenceText, t2.referenceText),
         shared_words(t1.referenceText, t2.referenceText),
         longest_substring(t1.referenceText, t2.referenceText)
-    ]
-    scaledX = MinMaxScaler.transform(scaler, X).astype(np.float)
-    return GradientBoostingRegressor.predict(model, scaledX)
+    ]]
+    scaled_X = scaler.transform(X).astype(np.float)
+    print(model.predict(scaled_X))
+    return model.predict(scaled_X)
